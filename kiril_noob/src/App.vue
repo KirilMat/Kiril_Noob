8fe858d0ce33ea2dea467e2271656010d16d1f82
KirilMat <matushok12@gmail.com>
2022-06-27 14:19:26 +0300

    What do u changes?

--- kiril_noob/src/App.vue
+++ kiril_noob/src/App.vue
@@ -1,14 +1,21 @@
 <template>
-
+ <div id="app">
+  <p>
+    Hello vue
+  </p>
+  <v-main-wrapper />
+ </div>
 </template>
 
 <script>
+
+  import vMainWrapper from './components/v-main-wrapper'
 
 
 export default {
   name: 'App',
   components: {
-  
+    vMainWrapper
   }
 }
 </script>
---
+++ kiril_noob/src/components/v-logo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="v-main-wrapper">
+        <p>{{title}}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "v-main-wrapper",
+        components: {},
+        props: {},
+        data() {
+            return {
+                title: 'Main wrapper'
+            }
+        },
+        computed: {},
+        methods: {},
+        watch: {},
+        mounted() {
+            console.log('Hello i am alive!')
+        }
+    }
+</script>
+
+<style scoped>
+    .v-main-wrapper{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        max-width: 900px;
+        margin: 0 auto;
+    }
+</style>
